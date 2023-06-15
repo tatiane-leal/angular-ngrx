ee1562733078a36cebc5c3d76b410f190ab63417
Tatiane Sala <tatianedleal@gmail.com>
2023-06-15 00:38:00 -0400

    update app module and add env files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MenuModule } from './components/menu/menu.module';
 import { InfoModule } from './components/info/info.module';
+import { TodoModule } from './components/todo/todo.module';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TodoEffects } from './state/todos/todos.effect';
+import { todoReducer } from './state/todos/todos.reducer';
+
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MenuModule, InfoModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenuModule,
+    InfoModule,
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+    EffectsModule.forRoot([TodoEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
