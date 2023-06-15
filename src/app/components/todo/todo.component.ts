3145bf34b21cd43593ce9758036b385afb0188a4
Tatiane Sala <tatianedleal@gmail.com>
2023-06-15 00:38:42 -0400

    update todo component

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { TodoService } from 'src/app/services/todo.service';
+import { Todo } from 'src/models/todo.interface';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  constructor(private _service: TodoService) {}
 
+  subscription: Subscription = new Subscription();
+  todos: Todo[] = [];
+
+  ngOnInit(): void {
+    this.subscription = this._service.fetchTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
+  }
 }
--- src/app/components/todo/todo.module.ts
+++ src/app/components/todo/todo.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { TodoService } from 'src/app/services/todo.service';
+
 import { TodoComponent } from './todo.component';
 
-
-
 @NgModule({
-  declarations: [
-    TodoComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TodoComponent],
+  imports: [CommonModule, HttpClientModule],
+  providers: [TodoService],
+  exports: [TodoComponent],
 })
-export class TodoModule { }
+export class TodoModule {}
